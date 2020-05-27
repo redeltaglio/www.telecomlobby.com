7dc4c4ae438ccff481edff2fae671df1453ccb17
Riccardo Giuntoli <taglio@ganesha.telecomlobby.net>
2020-05-27 06:34:42 +0000

    Wed May 27 06:34:37 UTC 2020

--- bin/gitpush.sh
+++ bin/gitpush.sh
@@ -2,6 +2,8 @@
 
 DATE=`date`
 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/riccardo_telecomlobby_rsa
 cd ~/Work/telecomlobby.com
 git add ~/Work/telecomlobby.com/bin ~/Work/telecomlobby.com/cgi ~/Work/telecomlobby.com/header ~/Work/telecomlobby.com/footer
 git commit -m "$DATE"
