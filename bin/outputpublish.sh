e3fab094990676d7b432bc6c7963fa7da567c265
Riccardo Giuntoli <taglio@ganesha.telecomlobby.net>
2020-05-28 07:41:26 +0000

    Thu May 28 07:41:19 UTC 2020

---
+++ bin/outputpublish.sh
@@ -0,0 +1,10 @@
+#!/bin/ksh
+
+OUTPUT="/home/taglio/Work/telecomlobby.com/output/"
+WWWOUTPUT="/var/www/htdocs/telecomlobby.com/test/"
+
+for html_file in $(ls $OUTPUT)
+do
+	doas cp $OUTPUT$html_file $WWWOUTPUT
+done
+./wwwperm.sh
--- bin/wwwperm.sh
+++ bin/wwwperm.sh
@@ -1,4 +1,4 @@
  #!/bin/ksh
 
 doas chown -R wwwftp:www /var/www/htdocs/*telecomlobby.com
-doas chmod -R o-rwx /var/www/htdocs/*telecomlobby.com
+doas chmod -R g+r,o-rwx /var/www/htdocs/*telecomlobby.com
