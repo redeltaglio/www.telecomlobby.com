b80c1a04dab450ca4730a3fae511fce9ff1c8707
Riccardo Giuntoli <taglio@ganesha.telecomlobby.net>
2020-05-31 11:35:32 +0000

    Sun May 31 11:35:27 UTC 2020

--- bin/gitpush.sh
+++ bin/gitpush.sh
@@ -6,7 +6,7 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/riccardo_telecomlobby_rsa
 cd ~/Work/telecomlobby.com
 git add ~/Work/telecomlobby.com/bin ~/Work/telecomlobby.com/cgi ~/Work/telecomlobby.com/header ~/Work/telecomlobby.com/footer ~/Work/telecomlobby.com/website
-git add ~/Work/telecomlobby.com/website/riccardo_giuntoli
+git add ~/Work/telecomlobby.com/website/riccardo_giuntoli ~/Work/telecomlobby.com/website/privacy
 git commit -m "$DATE"
 git push
 pkill -9 ssh-agent
