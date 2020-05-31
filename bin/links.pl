48e4d813bc8cb1cbfc1e5b4fec2e52659df75917
Riccardo Giuntoli <taglio@ganesha.telecomlobby.net>
2020-05-31 08:58:34 +0000

    Sun May 31 08:58:22 UTC 2020

--- bin/links.pl
+++ bin/links.pl
@@ -6,7 +6,7 @@ open INFILE, $ARGV[0] or die "Could not open file. $!";
 $string = <INFILE>;
 close INFILE;
 
-while ($string =~ s/\*\*(.*)\*\* \[\[\d+\]\]\((.*)\)/<a href="$2" rel="external nofollow">$1<\/a>/) {}
+while ($string =~ s/<strong>(.*)<\/strong> (.*)\[(\d+)\]<\/a>/$2 $1<\/a>/) {}
 
 print "$string";
 
