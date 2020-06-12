2c684a378fb9676868e491e828336811ac745ee7
Riccardo Giuntoli <taglio@ganesha.telecomlobby.net>
2020-06-12 06:34:46 +0000

    Fri Jun 12 06:34:35 UTC 2020

---
+++ bin/total.sh
@@ -0,0 +1,12 @@
+#!/bin/ksh
+
+sh ./publisher.sh riccardo_giuntoli www
+sh ./publisher.sh RNMnetwork www
+sh ./publisher.sh RNMnetwork/considerations www
+sh ./publisher.sh RNMnetwork/ES www
+sh ./publisher.sh RNMnetwork/ES/gangstalking www
+sh ./publisher.sh RNMnetwork/ES/get_access www
+sh ./publisher.sh RNMnetwork/technomafia www
+sh ./publisher.sh radioham
+sh ./publisher.sh RNMnetwork/technomafia/crimes_deaths_laws_health www
+sh ./publisher.sh RNMnetwork/electrosmog www
